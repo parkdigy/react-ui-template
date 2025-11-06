bc8016fbf61356376b27745e1f7239c9cebb919a
parkdigy <parkdigy@gmail.com>
2025-11-07 00:45:24 +0900

    Update

--- client/src/component/@Common/Layouts/Grid/Grid/Grid.types.ts
+++ client/src/component/@Common/Layouts/Grid/Grid/Grid.types.ts
@@ -1,10 +1,6 @@
 import { AllScreenAliases } from '@theme';
 import { CustomComponentFlexStyles } from '../../../CustomComponent';
 
-export interface GridInnerProps {
-  inner_num: number;
-}
-
 export type GridCols = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12;
 
 export interface GridProps extends Omit<BoxProps, 'display' | 'cols' | keyof CustomComponentFlexStyles> {
