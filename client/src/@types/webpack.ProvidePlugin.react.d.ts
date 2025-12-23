176b5a43dae822a3f50f94902c43cf3defdf702e
parkdigy <parkdigy@gmail.com>
2025-12-23 14:35:20 +0900

    Update

--- client/src/@types/webpack.ProvidePlugin.react.d.ts
+++ client/src/@types/webpack.ProvidePlugin.react.d.ts
@@ -1,11 +1,20 @@
+import { JSX } from 'react';
+
 declare type JSXElementConstructor<P> = import('react').JSXElementConstructor<P>;
 
 declare type Ref<T> = import('react').Ref<T>;
+
+declare type RefObject<T> = import('react').RefObject<T>;
 
 declare type ReactElement<
   P = unknown,
   T extends string | JSXElementConstructor<any> = string | JSXElementConstructor<any>,
 > = import('react').ReactElement<P, T>;
+
+declare type ElementType<
+  P = any,
+  Tag extends keyof JSX.IntrinsicElements = keyof JSX.IntrinsicElements,
+> = import('react').ElementType<P, Tag>;
 
 declare type ReactNode = import('react').ReactNode;
 
--- client/webpack/ProvidePlugin.react.js
+++ client/webpack/ProvidePlugin.react.js
@@ -1,7 +1,9 @@
 module.exports = {
   Ref: ['react', 'Ref'],
+  RefObject: ['react', 'RefObject'],
   ReactElement: ['react', 'ReactElement'],
   ReactNode: ['react', 'ReactNode'],
+  ElementType: ['react', 'ElementType'],
   ComponentType: ['react', 'ComponentType'],
   CSSProperties: ['react', 'CSSProperties'],
 };
