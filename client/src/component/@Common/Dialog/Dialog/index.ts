8f00b13fd18300c291e51df5c4a072443110fb68
parkdigy <parkdigy@gmail.com>
2025-12-25 05:26:10 +0900

    Update

--- client/src/component/@Common/Dialog/Dialog/index.ts
+++ client/src/component/@Common/Dialog/Dialog/index.ts
@@ -1,12 +1,8 @@
-import l from '@loadable/component';
-
-const Dialog = l(
-  () => import(/* webpackChunkName: "common-dialog" */ './Dialog'),
-  loadable.options
-) as unknown as typeof import('./Dialog').default;
-
-export { Dialog };
+import Dialog from './Dialog';
 
 export default Dialog;
 
+export { Dialog };
+
+export * from './Dialog';
 export * from './Dialog.types';
