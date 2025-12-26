7a6f47f7c4dc33127c5168d8e00554bc59c82424
parkdigy <parkdigy@gmail.com>
2025-12-26 15:04:50 +0900

    Update

--- client/webpack/ProvidePlugin.third-party.js
+++ client/webpack/ProvidePlugin.third-party.js
@@ -1,5 +1,14 @@
 module.exports = {
+  // styled-components
   styled: ['styled-components', 'default'],
+
+  // classnames
   classnames: ['classnames', 'default'],
+
+  // react-router
+  useLocation: ['react-router', 'useLocation'],
+  useSearchParams: ['react-router', 'useSearchParams'],
+
+  // usehooks-ts
   useWindowSize: ['usehooks-ts', 'useWindowSize'],
 };
