782b2d18f40e17f3b93341b98f3207e5493e709f
parkdigy <parkdigy@gmail.com>
2025-12-26 14:45:37 +0900

    Update

--- client/src/@types/webpack.ProvidePlugin.common-component.d.ts
+++ client/src/@types/webpack.ProvidePlugin.common-component.d.ts
@@ -1,12 +1,21 @@
 // Button
 declare const Button: typeof import('../component/@Common/index.ts').Button;
 declare type ButtonProps = import('../component/@Common/index.ts').ButtonProps;
+
+// Errors
+declare const ErrorBoundary: typeof import('../component/@Common/index.ts').ErrorBoundary;
+declare type ErrorBoundaryProps = import('../component/@Common/index.ts').ErrorBoundaryProps;
+declare const ErrorRetry: typeof import('../component/@Common/index.ts').ErrorRetry;
+declare type ErrorRetryProps = import('../component/@Common/index.ts').ErrorRetryProps;
+
 // Icon
 declare const Icon: typeof import('../component/@Common/index.ts').Icon;
 declare type IconProps = import('../component/@Common/index.ts').IconProps;
+
 // Img
 declare const Img: typeof import('../component/@Common/index.ts').Img;
 declare type ImgProps = import('../component/@Common/index.ts').ImgProps;
+
 // Layouts
 declare const Box: typeof import('../component/@Common/index.ts').Box;
 declare type BoxProps = import('../component/@Common/index.ts').BoxProps;
@@ -20,6 +29,12 @@ declare const Span: typeof import('../component/@Common/index.ts').Span;
 declare type SpanProps = import('../component/@Common/index.ts').SpanProps;
 declare const Stack: typeof import('../component/@Common/index.ts').Stack;
 declare type StackProps = import('../component/@Common/index.ts').StackProps;
+
+// Loadings
+declare const Loading: typeof import('../component/@Common/index.ts').Loading;
+declare type LoadingProps = import('../component/@Common/index.ts').LoadingProps;
+declare type LoadingCommands = import('../component/@Common/index.ts').LoadingCommands;
+
 // Texts
 declare const T: typeof import('../component/@Common/index.ts').T;
 declare type TProps = import('../component/@Common/index.ts').Props;
--- client/webpack/ProvidePlugin.common-component.js
+++ client/webpack/ProvidePlugin.common-component.js
@@ -4,12 +4,24 @@ module.exports = {
   // Button
   Button: [path.resolve(__dirname, '../src/component/@Common/Button/index.ts'), 'Button'],
   ButtonProps: [path.resolve(__dirname, '../src/component/@Common/Button/index.ts'), 'ButtonProps'],
+
+  // Errors
+  ErrorBoundary: [path.resolve(__dirname, '../src/component/@Common/Errors/ErrorBoundary/index.ts'), 'default'],
+  ErrorBoundaryProps: [
+    path.resolve(__dirname, '../src/component/@Common/Errors/ErrorBoundary/index.ts'),
+    'ErrorBoundaryProps',
+  ],
+  ErrorRetry: [path.resolve(__dirname, '../src/component/@Common/Errors/ErrorRetry/index.ts'), 'default'],
+  ErrorRetryProps: [path.resolve(__dirname, '../src/component/@Common/Errors/ErrorRetry/index.ts'), 'ErrorRetryProps'],
+
   // Icon
   Icon: [path.resolve(__dirname, '../src/component/@Common/Icon/index.ts'), 'Icon'],
   IconProps: [path.resolve(__dirname, '../src/component/@Common/Icon/index.ts'), 'IconProps'],
+
   // Img
   Img: [path.resolve(__dirname, '../src/component/@Common/Img/index.ts'), 'Img'],
   ImgProps: [path.resolve(__dirname, '../src/component/@Common/Img/index.ts'), 'ImgProps'],
+
   // Layouts
   Box: [path.resolve(__dirname, '../src/component/@Common/Layouts/Box/index.ts'), 'Box'],
   BoxProps: [path.resolve(__dirname, '../src/component/@Common/Layouts/Box/index.ts'), 'BoxProps'],
@@ -29,6 +41,12 @@ module.exports = {
   SpanProps: [path.resolve(__dirname, '../src/component/@Common/Layouts/Span/index.ts'), 'SpanProps'],
   Stack: [path.resolve(__dirname, '../src/component/@Common/Layouts/Stack/index.ts'), 'Stack'],
   StackProps: [path.resolve(__dirname, '../src/component/@Common/Layouts/Stack/index.ts'), 'StackProps'],
+
+  // Loadings
+  Loading: [path.resolve(__dirname, '../src/component/@Common/Loadings/Loading/index.ts'), 'default'],
+  LoadingProps: [path.resolve(__dirname, '../src/component/@Common/Loadings/Loading/index.ts'), 'LoadingProps'],
+  LoadingCommands: [path.resolve(__dirname, '../src/component/@Common/Loadings/Loading/index.ts'), 'LoadingCommands'],
+
   // Texts
   T: [path.resolve(__dirname, '../src/component/@Common/Texts/T/index.ts'), 'T'],
   TProps: [path.resolve(__dirname, '../src/component/@Common/Texts/T/index.ts'), 'TProps'],
